e18ac10513aae9cb99291ac948e0caca72031856
Haripriya G <927623bcs034@mkce.ac.in>
2026-04-14 10:13:51 +0530

    Remove API key from genai configuration
    Removed the API key from the configuration for security reasons.

--- img.py
+++ img.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 from PIL import Image
 img=Image.open(r"C:\Users\mguna\Downloads\image.avif")
-genai.configure(api_key="AIzaSyAMbYMsot49vBAMKoDMddEofRiNyJtKYzk")
+genai.configure(api_key="")
 model=genai.GenerativeModel("gemini-2.5-flash")
 prompt=input("Enter the Questions:")            
 response=model.generate_content([prompt,img])        
